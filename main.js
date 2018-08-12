7dcc1bd4bfedcd77eacdf0019a459542d0953b2b
Mario Borras <mario.borras@icloud.com>
2018-08-12 17:52:01 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+  $.ajax({
+    type: 'GET',
+    url: "http://www.redbullshopus.com/products.json",
+  data: {
+    },
+   success: function(response) {
+      for(var i = 0; i < response.products.length; i++){
+       
+
+        $('#main').append("<div class='interior'>"+
+          "<div class ='photos'>" + "<img width='250' height='250' src="+ response.products[i].images[0].src +"</div>" +
+        "<div class='product'>" + response.products[i].title + "</div>"+
+        "<div class ='price'>" +"$"+ response.products[i].variants[0].price +"<br><button>Add to Cart</button>"+
+        "</div></div>")
+
+      }
+    }
+  })
+
