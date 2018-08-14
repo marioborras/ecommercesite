5d059431194819aa433d675cb37a28ae5b33e8cb
Mario Borras <mario.borras@icloud.com>
2018-08-14 10:08:45 -0400

    added div to center

--- main.js
+++ main.js
@@ -8,11 +8,11 @@
       for(var i = 0; i < response.products.length; i++){
        
 
-        $('#main').append("<div class='interior'>"+
+        $('#main').append("<div id='centeringDiv'>"+"<div class='interior'>"+
           "<div class ='photos'>" + "<img width='250' height='250' src="+ response.products[i].images[0].src +"</div>" +
         "<div class='product'>" + response.products[i].title + "</div>"+
         "<div class ='price'>" +"$"+ response.products[i].variants[0].price +"<br><button>ADD TO CART</button>"+
-        "</div></div>")
+        "</div></div></div>")
 
       }
     }
